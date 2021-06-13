479dfde3bd74cb81d33a8b002823a2aec2d7097d
dobachi <dobachi1983oss@gmail.com>
2021-06-13 21:30:38 +0900

    Rename the helper script and add importing environmental variables.

--- bin/pyspark_jupyter.sh
+++ bin/pyspark_jupyter.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+cd ${SCRIPT_DIR}/..
+. .profile
+
 export PYSPARK_DRIVER_PYTHON="jupyter"
 export PYSPARK_DRIVER_PYTHON_OPTS="lab --ip=0.0.0.0 --no-browser"
 export AWS_PROFILE=delta_sharing
 export SPARK_LOCAL_IP=localhost
 
-PYSPARK_BIN=/usr/local/spark/default/bin/pyspark
+PYSPARK_BIN=${PYSPARK_BIN:-/usr/local/spark/default/bin/pyspark}
+
 ${PYSPARK_BIN} --packages org.apache.hadoop:hadoop-aws:3.2.0,org.apache.hadoop:hadoop-common:3.2.0,io.delta:delta-core_2.12:1.0.0 \
   --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog" \
   --conf "spark.hadoop.fs.s3a.aws.credentials.provider=com.amazonaws.auth.profile.ProfileCredentialsProvider"
